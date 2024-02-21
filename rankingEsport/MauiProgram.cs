619219177786f5d322ef772811efb44f8272946a
Alhai <aly.haidara@seira-formation.fr>
2024-02-21 15:55:43 +0100

    refacto form addPlayer

--- rankingEsport/MauiProgram.cs
+++ rankingEsport/MauiProgram.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.Logging;
-using Telerik.Maui.Controls.Compatibility;
 
 namespace rankingEsport;
 
@@ -10,7 +9,6 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
-             .UseTelerik()
             .ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
