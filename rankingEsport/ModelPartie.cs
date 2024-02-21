e771094d0323b46ca0fa04075b72cd791eb7c636
frazi <frazierjeremy1@outlook.fr>
2024-02-21 13:43:07 +0100

    modelPartie

---
+++ rankingEsport/ModelPartie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class ModelGames
+{
+	public ModelGames()
+	{
+		public int id {  get; set; },
+		public string name { get; set; },
+		public string members { get; set; },
+	    public int statisticTeams { get; set; }
+
+	}
+}
