5428788038c9d8af2b17946cb535f918b2079bc0
zahra4221 <shakilzahra1@gmail.com>
2024-02-21 22:47:03 +0100

    interface Iplayer

--- rankingEsport/Domains/Player.cs
+++ rankingEsport/Domains/Player.cs
@@ -3,21 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using rankingEsport.Interfaces;
 
 namespace rankingEsport.Domains
-{
-    public class Player
+{ 
+    public class Player : IPlayer
     {
         public int ID { get; set; }
         public required string Name { get; set; }
         public required string Pseudo { get; set; }
         public required Teams Team { get; set; }
-        public required string Statistics { get; set; }
+        public required  string Statistics { get; set; }
     }
+
     public class Teams
     {
         public int ID { get; set; }
         public required string Name { get; set; }
-
     }
 }
---
+++ rankingEsport/Interfaces/IPlayer.cs
@@ -0,0 +1,19 @@
+﻿using rankingEsport.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rankingEsport.Interfaces
+{
+    interface IPlayer
+
+    {
+        int ID { get; set; }
+        string Name { get; set; }
+        string Pseudo { get; set; }
+        Teams Team { get; set; }
+        string Statistics { get; set; }
+    }
+}
