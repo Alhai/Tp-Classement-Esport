d9617b6484c42694183b5f0783da8facd5348d78
Alhai <aly.haidara@seira-formation.fr>
2024-02-21 12:24:25 +0100

    Addmodelplayer

--- rankingEsport/PlayerModel.cs
+++ rankingEsport/PlayerModel.cs
@@ -3,9 +3,12 @@ namespace rankingEsport
 {
 	public class PlayerModel
 	{
-		public PlayerModel()
-		{
-		}
+		public string ID { get; set; }
+		public string Name { get; set; }
+		public string Pseudo { get; set; }
+		public string Team { get; set; }
+		public int Statistic { get; set; }
+
 	}
 }
 
