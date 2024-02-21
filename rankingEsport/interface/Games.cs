b2dcb46e5d594657a053b1cccadceec8146cfaeb
frazi <frazierjeremy1@outlook.fr>
2024-02-21 22:46:21 +0100

    suite

---
+++ rankingEsport/interface/Games.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rankingEsport.Domains
+{
+    public class Player
+    {
+        public int ID { get; set; }
+        public required string Name { get; set; }
+        public required string Pseudo { get; set; }
+        public required Teams Team { get; set; }
+        public required string Statistics { get; set; }
+    }
+    public class Teams
+    {
+        public int ID { get; set; }
+        public required string Name { get; set; }
+
+    }
+
+    public class Games
+    {
+        public int ID { get; set; }
+        public required DateTime Date { get; set; }
+
+        public required string NameGame { get; set; }
+
+        public required Teams team {  get; set; }
+
+        public required int Score { get; set; }
+
+        public required string Statistics { get; set; }
+    }
+
+   
+}
