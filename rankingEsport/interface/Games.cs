513f7a94ba1ce412d35c082f6b80cda566d0556b
frazi <frazierjeremy1@outlook.fr>
2024-02-22 09:00:44 +0100

    correction

--- rankingEsport/interface/Games.cs
+++ rankingEsport/interface/Games.cs
@@ -6,20 +6,7 @@ using System.Threading.Tasks;
 
 namespace rankingEsport.Domains
 {
-    public class Player
-    {
-        public int ID { get; set; }
-        public required string Name { get; set; }
-        public required string Pseudo { get; set; }
-        public required Teams Team { get; set; }
-        public required string Statistics { get; set; }
-    }
-    public class Teams
-    {
-        public int ID { get; set; }
-        public required string Name { get; set; }
-
-    }
+   
 
     public class Games
     {
