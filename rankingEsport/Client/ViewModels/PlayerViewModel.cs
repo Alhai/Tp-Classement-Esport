98046f45e375e48035c96cef78cbea7b42fd67f5
Alhai <aly.haidara@seira-formation.fr>
2024-02-22 15:49:55 +0100

    WIP

--- rankingEsport/Client/ViewModels/PlayerViewModel.cs
+++ rankingEsport/Client/ViewModels/PlayerViewModel.cs
@@ -26,20 +26,67 @@ namespace rankingEsport.ViewModels
 
         public ICommand AddPlayerCommand { get; private set; }
 
-        public PlayerViewModel(PlayerService playerService)
+        private int id;
+        public int Id
         {
-            this.playerService = playerService;
+            get { return id; }
+            set { id = value; OnPropertyChanged(); }
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; OnPropertyChanged(); }
+        }
+
+        private string pseudo;
+        public string Pseudo
+        {
+            get { return pseudo; }
+            set { Pseudo = value; OnPropertyChanged(); }
+        }
+
+        private string team;
+        public string Team
+        {
+            get { return team; }
+            set { team = value; OnPropertyChanged(); }
+        }
+        private string statistic;
+        public string Statistic
+        {
+            get { return statistic; }
+            set { statistic = value; OnPropertyChanged(); }
+        }
+        public List<string> Teams { get; set; } = new List<string>()
+        {
+            "Droïde de protocole",
+            "Droïde astromécano",
+        };
+
+
+        public PlayerViewModel()
+        {
+            this.playerService = new PlayerService(new PlayerPersistence());
             this.Players = new ObservableCollection<PlayerModel>();
-            this.AddPlayerCommand = new RelayCommand(AddPlayer);
+            this.AddPlayerCommand = new Command(AddPlayer);
 
         }
+        //public PlayerViewModel(PlayerService playerService)
+        //{
+        //    this.playerService = playerService;
+        //    this.Players = new ObservableCollection<PlayerModel>();
+        //    this.AddPlayerCommand = new Command(AddPlayer);
+
+        //}
+
         private void AddPlayer(object obj)
         {
             var player = new PlayerModel { Name = "Nom", Pseudo = "Pseudo", Team = "Team", Statistic = 0 };
             playerService.AddPlayer(player);
             Players.Add(player);
         }
-
     
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
