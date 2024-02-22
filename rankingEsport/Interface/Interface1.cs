3aa5c999f19beb3c837fc28ac17b33b3f9bbd092
frazi <frazierjeremy1@outlook.fr>
2024-02-22 09:38:29 +0100

    debut IGames

---
+++ rankingEsport/Interface/Interface1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rankingEsport.Interface
+{
+     interface IGames
+    {
+        int ID { get; set; }
+        DateTime Date { get; set; }
+        string Name {  get; set; }
+        Teams teams { get; set; }
+        int Score { get; set; }
+        string Statistiques { get; set; }
+
+        void SaveStatisticGames();
+        void SaveStatisticPlayers();
+        void SaveStatisticteams();
+        void 
+    }
+}
--- rankingEsport/interface/Games.cs
+++ rankingEsport/interface/Games.cs
@@ -4,9 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace rankingEsport.Domains
+namespace rankingEsport.Interface
 {
-   
+    public class Player
+    {
+        public int ID { get; set; }
+        public required string Name { get; set; }
+        public required string Pseudo { get; set; }
+        public required Teams Team { get; set; }
+        public required string Statistics { get; set; }
+    }
+    public class Teams
+    {
+        public int ID { get; set; }
+        public required string Name { get; set; }
+
+    }
 
     public class Games
     {
