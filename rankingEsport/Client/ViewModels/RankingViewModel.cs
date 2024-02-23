2045844196df72abbe04caaf92f327b5f4c1525d
frazi <frazierjeremy1@outlook.fr>
2024-02-23 14:04:06 +0100

    ajout des rangs de 1 à 6

--- rankingEsport/Client/ViewModels/RankingViewModel.cs
+++ rankingEsport/Client/ViewModels/RankingViewModel.cs
@@ -1,6 +1,4 @@
-﻿using rankingEsport.Client.Models;
-using rankingEsport.Server.Services;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -8,6 +6,15 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Xml.Linq;
+using rankingEsport.Client.Models;
+using rankingEsport.Models;
+using rankingEsport.Persistances;
+using rankingEsport.Server.Persistences;
+using rankingEsport.Server.Services;
+using rankingEsport.Services;
+using rankingEsport.ViewModels;
 
 namespace rankingEsport.Client.ViewModels
 {
@@ -45,7 +52,16 @@ namespace rankingEsport.Client.ViewModels
             get { return totalpoint; }
             set { totalpoint = value; OnPropertyChanged(); }
         }
-
+        public RankingViewModel()
+        {
+            this.rankingService = new RankingService(new RankingPersistence());
+            this.Rankings = new ObservableCollection<RankingModel>();
+        }
+        public List<int> Ranks { get; set; } = new List<int>()
+        {
+            1, 2, 3,
+            4, 5, 6
+        };
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
