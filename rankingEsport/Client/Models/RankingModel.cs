5dc1bebe2e75f8a2c6319ca0e298d93e0dece406
frazi <frazierjeremy1@outlook.fr>
2024-02-23 08:56:35 +0100

    modif

---
+++ rankingEsport/Client/Models/RankingModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rankingEsport.Client.Models
+{
+    public class RankingModel
+    {
+        public int Rank { get; set; }
+        public required string Team { get; set; }
+        public int WinRate { get; set; }
+        public int TotalPoint { get; set; }
+    }
+}
